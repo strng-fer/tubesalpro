7236e117ccc1ee533e80997e7881eb50ad3c53a0
Feryadi Yulius <133149348+strng-fer@users.noreply.github.com>
2023-11-19 22:42:53 +0700

    Update macning.py

--- macning.py
+++ macning.py
@@ -1,56 +1,14 @@
 import streamlit as st
-import requests
-
-# Fungsi untuk verifikasi login
-def verify_login(username, password):
-    # Ganti URL dengan URL file txt di GitHub yang berisi username dan password
-    url = 'https://raw.githubusercontent.com/strng-fer/tubesalpro/main/user.txt'
-    response = requests.get(url)
-    credentials = response.text.split('\n')
-    
-    for cred in credentials:
-        if ',' in cred:
-            stored_username, stored_password = cred.split(',')
-            if username == stored_username and password == stored_password:
-                return True
-    
-    return False
-
-def homepage():
-    st.title("Journey Mancing - Halaman Utama")
-    st.write("Selamat datang di Halaman Utama! Silakan lanjutkan dengan aktivitas Anda di sini.")
+from apps import login_app, homepage_app
 
 def main():
-    st.title("Journey Mancing")
-    st.markdown(
-        """
-        <style>
-        .reportview-container {
-            background: url('https://p4.wallpaperbetter.com/wallpaper/600/885/315/fishing-astronaut-person-fishing-in-crack-area-illustration-wallpaper-preview.jpg?t=1');
-            background-size: cover;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", ("Login", "Homepage"))
 
-    login_container = st.empty()
-
-    # Inisialisasi variabel global untuk menyimpan status login
-    if 'is_logged_in' not in st.session_state:
-        st.session_state.is_logged_in = False
-
-    if not st.session_state.is_logged_in:
-        st.title("Login")
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-
-        if st.button("Login"):
-            if verify_login(username, password):
-                st.success("Login berhasil!")
-                st.session_state.is_logged_in = True  # Set status login menjadi True setelah berhasil login
-                login_container.empty()  # Sembunyikan halaman login setelah login berhasil
-                homepage()  # Tampilkan homepage setelah login berhasil
+    if selection == "Login":
+        login_app.run()
+    elif selection == "Homepage":
+        homepage_app.run()
 
 if __name__ == "__main__":
     main()
